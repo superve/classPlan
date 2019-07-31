5157f2b72ad21eb95b97b3940c119052b120c7eb
gogery <gogery@163.com>
2019-07-31 18:22:08 +0800

    复合组件

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,106 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class PanelHeader extends React.Component{
+  render(){
+    return(
+<div className="panel-heading">
+            <h3 className="panel-title"> {this.props.name}</h3>
+          </div>
+    )
+  }
 }
 
-export default App;
+class PanelBody extends React.Component{
+  static defaultProps = {
+    color: 'default'
+  }
+  render(){
+    return(
+      <div className="panel-body">
+            {this.props.name}-{this.props.color}
+          </div>
+    )
+  }
+}
+
+class PanelFooter extends React.Component{
+  render(){
+    return (
+      <div className="panel-footer">
+      {this.props.name}
+    </div>
+    )
+  }
+}
+class Pannel extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      color: '',
+      body: 'body'
+    }
+  }
+  handleChaneColorDanger = () => {
+    console.log('红色');
+    // this.setState({color: 'red'});
+   
+  
+    this.setState(function (prevState, prors) {
+      console.log( prevState );
+      console.log( prors );
+      
+      
+    })
+    
+  }
+
+  handleChaneColorSuccess =() => {
+    console.log('绿色');
+    this.setState({color: 'green'})
+
+
+  }
+
+  render() {
+    return (
+
+      <div>
+        
+        <button type="button" className="btn btn-danger" onClick={
+          this.handleChaneColorDanger
+        }>变红色</button>
+        
+        <button type="button" className="btn btn-success" onClick={
+           this.handleChaneColorSuccess
+        }>变绿色</button>
+        
+        
+        <div className="panel panel-danger">
+          <PanelHeader name="头部"></PanelHeader>
+          <PanelBody name="身体" color={this.state.color}></PanelBody>
+          <PanelFooter name='底部'></PanelFooter>
+        </div>
+
+      </div>
+
+    )
+
+  }
+}
+
+
+export default class Container extends React.Component {
+
+  render() {
+    return (
+
+      <div className="container">
+        <h1>React基础学习</h1>
+        <hr />
+        <Pannel name="panel" />
+
+      </div>
+    )
+  }
+}
