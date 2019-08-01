d8d73347085dbd1792bde7383fc3f89fcbbf7693
gogery <gogery@163.com>
2019-08-01 18:03:30 +0800

    增加前端todo的测试接口

---
+++ my-app/Btn.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Btn extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
--- readme.md
+++ readme.md
@@ -4,3 +4,9 @@
 3. react-todo案例已经完成
 4. react-router在准备中
 5. redux已经完成
+
+
+
+# todo常见接口
+1. https://via.placeholder.com/728x90/f0f.png?text=jishufun.com
+2. http://jsonplaceholder.typicode.com
